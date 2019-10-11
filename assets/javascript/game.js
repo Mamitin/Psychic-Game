2ce6be01401201bfc6c25faeca3f5b97d242f526
Monica Amitin <monzielvsu@gmail.com>
2019-10-10 17:38:27 -0700

    Made adjustments to game.js, reorganized the text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,54 @@
 
 
 // Create vars
-var usersChoice = [];
 var wins = 0;
-var guessesLeft = 10;
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var computerGuess = "";
-var userGuess = "";
 var loses = 0;
+var guessesLeft = 10;
+var usersChoice = [];
+var computerGuess = "";
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//var userGuess = "";
+
+computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+//console.log(computerChoices);
+
+function guessPl() {
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+//console.log(computerChoices);
+}
+
 
 //userGuess
 document.onkeyup = function (event) {
-    console.log("onkeyup works")
-    //set computer auto generated guess using math.random()
-    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    console.log(computerGuess);
-    //capture userGuess
-    function guessesSoFar() {
-        document.querySelector("letter").innerHTML = "Your guesses so far: " + letterUser.join();
-    }
     userGuess = event.key;
-    console.log(userGuess);
-    console.log(event);
-    // function to compare userGuess and ComputerChoices
-    //if computerGuess is not equal to userGuess = false //compare guesses
+    //console.log("onkeyup works")
     if (computerGuess == userGuess) {
         wins++;
+        guessesLeft;
+        usersChoice;
     } else {
         loses++;
         //loses counted
         //guesses left - decrease
-        function countGuessesLeft() {
-            document.querySelector("guessesLeft").innerHTML = "Guesses Left: " + letterUser.join('');
-            console.log(countGuessesLeft)
-        }
-
     }
+
+
+
+
+    //set computer auto generated guess using math.random()
+    //capture userGuess
+    function guessesSoFar() {
+        document.querySelector("letter").innerHTML = "Your guesses so far: " + letterUser.join();
+    }
+    console.log(userGuess);
+    console.log(event);
+    // function to compare userGuess and ComputerChoices
+    //if computerGuess is not equal to userGuess = false //compare guesses
+
+}
+function countGuessesLeft() {
+    document.querySelector("guessesLeft").innerHTML = "Guesses Left: " + letterUser.join('');
+    console.log(countGuessesLeft)
 }
 
 
